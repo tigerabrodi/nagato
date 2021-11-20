03bc124278176c789f2a9ec497ddf8da2ae055da
tigerabrodi <tigerabrodi@gmail.com>
2021-11-20 13:10:17 +0100

    setup toast

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,18 @@
 import { globalStyles } from '@theme/globalStyles'
+import { toastOptions } from '@theme/toastOptions'
 import type { AppProps } from 'next/app'
+import { Toaster } from 'react-hot-toast'
 
 export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   globalStyles()
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Toaster
+        position="top-center"
+        toastOptions={toastOptions}
+        containerStyle={{ top: 50 }}
+      />
+    </>
+  )
 }
