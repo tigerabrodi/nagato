d1e44962419f009bd59b28c0502862ea5a033490
tigerabrodi <tigerabrodi@gmail.com>
2021-11-21 08:41:39 +0100

    chore(stitches): add utils and shadows

--- stitches.config.ts
+++ stitches.config.ts
@@ -1,5 +1,7 @@
 import { createStitches } from '@stitches/react'
 
+const SHADOW_COLOR = '285deg 6% 9%'
+
 export const {
   config,
   createTheme,
@@ -15,6 +17,10 @@ export const {
       secondary: '#FF0073',
       tertiary: '#FFFFFF',
     },
+    shadows: {
+      shadowElevationMedium: `0px 0.1px 0.1px hsl(${SHADOW_COLOR} / 0.09), 0px 0.7px 1px -0.2px hsl(${SHADOW_COLOR} / 0.26), 0px 1.5px 2.1px -0.3px hsl(${SHADOW_COLOR} / 0.44), 0px 3.3px 4.7px -0.5px hsl(${SHADOW_COLOR} / 0.61)`,
+      shadowElevationLow: `0px 0.8px 1px hsl(${SHADOW_COLOR} / 0.29), 0px 1.6px 2.1px -0.7px hsl(${SHADOW_COLOR} / 0.44), 0px 3.9px 5px -1.4px hsl(${SHADOW_COLOR} / 0.6);`,
+    },
     fontWeights: {
       regular: 400,
       medium: 500,
@@ -28,6 +34,24 @@ export const {
     fonts: {
       Inter: '"Inter", sans-serif',
     },
+  },
+  utils: {
+    marginX: (value: number) => ({
+      marginLeft: value,
+      marginRight: value,
+    }),
+    marginY: (value: number) => ({
+      marginTop: value,
+      marginBottom: value,
+    }),
+    paddingX: (value: number) => ({
+      paddingLeft: value,
+      paddingRight: value,
+    }),
+    paddingY: (value: number) => ({
+      paddingTop: value,
+      paddingBottom: value,
+    }),
   },
   media: {
     tablet: `(min-width: ${550 / 16}rem)`,
