f363bb78d1dc97a8abc8036f637cb9ab265b6e6d
tigerabrodi <tigerabrodi@gmail.com>
2021-11-22 07:58:26 +0100

    fix: calculate mobile height within resize event

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,8 +11,13 @@ export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
 
   /* Calculate viewport height for mobile */
   React.useEffect(() => {
-    let vh = window.innerHeight * 0.01
-    document.documentElement.style.setProperty('--vh', `${vh}px`)
+    function handleResize() {
+      let vh = window.innerHeight * 0.01
+      document.documentElement.style.setProperty('--vh', `${vh}px`)
+    }
+
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
   }, [])
 
   return (
