5a4e9b6bb5a55280b0927c71c21dbff09e9ea0c0
tigerabrodi <tigerabrodi@gmail.com>
2021-11-23 09:53:32 +0100

    feat(useAuth): hook to check if user is authenticated

---
+++ hooks/useAuth.ts
@@ -0,0 +1,47 @@
+import * as React from 'react'
+import { supabase } from '@lib/client'
+import { useRouter } from 'next/router'
+import { AuthChangeEvent, Session } from '@supabase/gotrue-js'
+
+export const useAuth = () => {
+  const router = useRouter()
+  const [isAuthenticated, setIsAuthenticated] = React.useState(false)
+
+  React.useEffect(() => {
+    const { data: authListener } = supabase.auth.onAuthStateChange(
+      (event, session) => {
+        handleAuthChange(event, session)
+        if (event === 'SIGNED_IN') {
+          setIsAuthenticated(true)
+        }
+        if (event === 'SIGNED_OUT') {
+          setIsAuthenticated(false)
+          router.push('/')
+        }
+      }
+    )
+    checkUser()
+    return () => {
+      authListener?.unsubscribe()
+    }
+  }, [router])
+
+  function checkUser() {
+    const user = supabase.auth.user()
+    setIsAuthenticated(Boolean(user))
+  }
+
+  async function handleAuthChange(
+    event: AuthChangeEvent,
+    session: Session | null
+  ) {
+    await fetch('/api/auth', {
+      method: 'POST',
+      headers: new Headers({ 'Content-Type': 'application/json' }),
+      credentials: 'same-origin',
+      body: JSON.stringify({ event, session }),
+    })
+  }
+
+  return { isAuthenticated }
+}
