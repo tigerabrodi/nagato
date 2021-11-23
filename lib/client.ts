ca9fbe3033ca7e52127e81f6370faf711ce4d3b6
tigerabrodi <tigerabrodi@gmail.com>
2021-11-23 09:50:02 +0100

    feat: setup supabase

---
+++ lib/client.ts
@@ -0,0 +1,9 @@
+import { createClient } from '@supabase/supabase-js'
+
+// Create a single supabase client for interacting with your database
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+)
+
+export { supabase }
