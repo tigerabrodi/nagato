8c08799dc98f0f4eff5c9f83daab108a752d2bcb
tigerabrodi <tigerabrodi@gmail.com>
2021-11-23 12:25:44 +0100

    test(sign-up): minimally assert user is on the edit page after signing up

--- cypress/e2e/sign-up.ts
+++ cypress/e2e/sign-up.ts
@@ -18,12 +18,9 @@ it('Should be able to sign up', () => {
     `Congrats ${user.fullname}, you successfully signed up!`
   ).should('exist')
 
-  // Should be on edit profile page
-  cy.findByRole('heading', { level: 1, name: 'Edit Your Profile' }).should(
+  // Should be on edit profile page and authenticated
+  cy.findByRole('link', { name: 'To Profile' }).should('exist')
+  cy.findByRole('heading', { level: 1, name: 'Edit your profile' }).should(
     'exist'
   )
-  cy.findByRole('heading', { level: 2, name: user.fullname }).should('exist')
-  cy.findByLabelText('Taste of music')
-  cy.findByLabelText('Avatar Upload').should('exist')
-  cy.findByRole('img', { name: 'Default Avatar' }).should('exist')
 })
