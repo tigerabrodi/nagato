76b71d5192bc5d9a2c270c6590ec9170302501d6
tigerabrodi <tigerabrodi@gmail.com>
2021-11-23 19:48:25 +0100

    test: sign in validation

---
+++ cypress/e2e/sign-in.ts
@@ -0,0 +1,27 @@
+import { buildUser, narutoTestUser } from '../support/generate'
+
+beforeEach(() => {
+  indexedDB.deleteDatabase('firebaseLocalStorageDb')
+  cy.visit('/sign-in')
+})
+
+it('Should not be able to submit if any fields are empty.', () => {
+  const user = buildUser()
+
+  cy.findByLabelText('Email*').type(user.email)
+
+  cy.findByRole('button', { name: 'Sign In' }).click()
+
+  cy.findByText('Please fill out both fields.').should('exist')
+})
+
+it('Should not sign in if anything goes wrong such as password', () => {
+  const user = buildUser()
+
+  cy.findByLabelText('Email*').type(narutoTestUser.email)
+  cy.findByLabelText('Password*').type(user.password)
+
+  cy.findByRole('button', { name: 'Sign In' }).click()
+
+  cy.findByText('Email or password is invalid.').should('exist')
+})
