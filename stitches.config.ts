77bab2746497d24712832d7a5e79f978415398de
tigerabrodi <tigerabrodi@gmail.com>
2021-11-24 16:57:37 +0100

    adjust shadow and typings of config

--- stitches.config.ts
+++ stitches.config.ts
@@ -20,7 +20,7 @@ export const {
     shadows: {
       shadowElevationMedium: `0px 0.1px 0.1px hsl(${SHADOW_COLOR} / 0.09), 0px 0.7px 1px -0.2px hsl(${SHADOW_COLOR} / 0.26), 0px 1.5px 2.1px -0.3px hsl(${SHADOW_COLOR} / 0.44), 0px 3.3px 4.7px -0.5px hsl(${SHADOW_COLOR} / 0.61)`,
       shadowElevationLow: `0px 0.8px 1px hsl(${SHADOW_COLOR} / 0.29), 0px 1.6px 2.1px -0.7px hsl(${SHADOW_COLOR} / 0.44), 0px 3.9px 5px -1.4px hsl(${SHADOW_COLOR} / 0.6);`,
-      shadowTabletMedium: '0 2px 3px black',
+      shadowMedium: '0 2px 3px black',
     },
     fontWeights: {
       regular: 400,
@@ -37,7 +37,7 @@ export const {
     },
   },
   utils: {
-    heightWidth: (value: number | `min(${string})`) => ({
+    heightWidth: (value: number | `min(${string})` | '100%') => ({
       height: value,
       width: value,
     }),
