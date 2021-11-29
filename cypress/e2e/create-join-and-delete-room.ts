9145d29a48ea0c88ef4a7d1aec08c1202604166f
tigerabrodi <tigerabrodi@gmail.com>
2021-11-29 19:15:46 +0100

    test: more comments and destructure user

--- cypress/e2e/create-join-and-delete-room.ts
+++ cypress/e2e/create-join-and-delete-room.ts
@@ -6,64 +6,58 @@ beforeEach(() => {
 })
 
 it('Create room, leave and join via rooms page, paste ID into join dialog and join room.', () => {
+  const { email, fullname, password, roomTypeOfMusic, roomTitle } =
+    narutoTestUser
+
   // Sign in as naruto
-  cy.findByLabelText('Email*').type(narutoTestUser.email)
-  cy.findByLabelText('Password*').type(narutoTestUser.password)
+  cy.findByLabelText('Email*').type(email)
+  cy.findByLabelText('Password*').type(password)
   cy.findByRole('button', { name: 'Sign In' }).click()
 
   // Create a room
   cy.findByRole('button', { name: 'Create Room' }).click()
   cy.findByRole('dialog', { name: 'Create Room' }).within(() => {
-    cy.findByLabelText('Title').type(narutoTestUser.roomTitle)
-    cy.findByLabelText('Type of music').type(narutoTestUser.roomTypeOfMusic)
+    cy.findByLabelText('Title').type(roomTitle)
+    cy.findByLabelText('Type of music').type(roomTypeOfMusic)
     cy.findByRole('button', { name: 'Create' }).click()
   })
 
-  // Assert the initial room detail page
+  // Toast message and copy room ID to clipboard
   cy.findByText(
-    `Room ${narutoTestUser.roomTitle} have successfully been created! ID of room has been copied to clipboard!`
+    `Room ${roomTitle} have successfully been created! ID of room has been copied to clipboard!`
   ).should('exist')
 
-  cy.findByRole('heading', { level: 1, name: narutoTestUser.roomTitle }).should(
-    'exist'
-  )
-  cy.findByText(narutoTestUser.roomTypeOfMusic).should('exist')
-  cy.findByRole('link', { name: `By ${narutoTestUser.fullname}` }).should(
-    'exist'
-  )
-  cy.findByRole('img', { name: narutoTestUser.fullname }).should('exist')
+  // Assert the initial room detail page
+  cy.findByRole('heading', { level: 1, name: roomTitle }).should('exist')
+  cy.findByText(roomTypeOfMusic).should('exist')
+  cy.findByRole('link', { name: `By ${fullname}` }).should('exist')
+  cy.findByRole('img', { name: fullname }).should('exist')
 
-  cy.findByRole('heading', { level: 2, name: 'Participants' }).should('exist')
-
+  // Currently no song should be playing
   cy.findByText('Currently no song is being played.').should('exist')
 
   // Authorized buttons should be visible
   cy.findByRole('button', { name: 'Search' }).should('exist')
   cy.findByRole('button', { name: 'Delete' }).should('exist')
 
+  // Copy ID button should be visible
   cy.findByRole('button', { name: 'Copy ID' }).should('exist')
 
   // Go to the rooms page
   cy.findByRole('link', { name: 'To Rooms' }).click()
 
   // Find the room as item to join
-  cy.findByRole('heading', { level: 2, name: narutoTestUser.roomTitle }).should(
-    'exist'
-  )
-  cy.findByText(narutoTestUser.roomTypeOfMusic).should('exist')
-  cy.findByRole('link', { name: `By ${narutoTestUser.fullname}` }).should(
-    'exist'
-  )
+  cy.findByRole('heading', { level: 2, name: roomTitle }).should('exist')
+  cy.findByText(roomTypeOfMusic).should('exist')
+  cy.findByRole('link', { name: `By ${fullname}` }).should('exist')
 
   // Join the room
   cy.findByRole('link', {
-    name: `Join room ${narutoTestUser.roomTitle}`,
+    name: `Join room ${roomTitle}`,
   }).click()
 
   // Should be in room
-  cy.findByRole('heading', { level: 1, name: narutoTestUser.roomTitle }).should(
-    'exist'
-  )
+  cy.findByRole('heading', { level: 1, name: roomTitle }).should('exist')
 
   // Copy ID to clipboard
   cy.findByRole('button', { name: 'Copy ID' }).click()
@@ -75,18 +69,18 @@ it('Create room, leave and join via rooms page, paste ID into join dialog and jo
   // Join the room via dialog
   cy.findByRole('button', { name: 'Join Room' }).click()
   cy.findByRole('dialog', { name: 'Join Room' }).within(() => {
-    cy.findByLabelText('Room ID').trigger('paste')
+    cy.findByLabelText('Room ID').focus().trigger('paste')
     cy.findByRole('button', { name: 'Join' }).click()
   })
 
   // Toast to appear when joining a room
-  cy.findByText(
-    `You successfully joined the room ${narutoTestUser.roomTitle}!`
-  ).should('exist')
+  cy.findByText(`You successfully joined the room ${roomTitle}!`).should(
+    'exist'
+  )
 
   // Delete the room
   cy.findByRole('button', { name: 'Delete' }).click()
-  cy.findByText(`Room ${narutoTestUser.roomTitle} was deleted!`).should('exist')
+  cy.findByText(`Room ${roomTitle} was deleted!`).should('exist')
 
   // Should be on rooms page
   cy.findByRole('heading', { level: 1, name: 'Rooms' }).should('exist')
