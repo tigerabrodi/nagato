b5b32b3a0c5fb6806619d82533b6a1ffe48e2c5d
tigerabrodi <tigerabrodi@gmail.com>
2021-12-02 20:57:31 +0100

    feat(RoomItem): create component

---
+++ components/RoomItem/index.tsx
@@ -0,0 +1,45 @@
+import { Room } from '@lib/types'
+import Link from 'next/link'
+import {
+  MusicalNote,
+  RoomAuthorLink,
+  RoomItemHeading,
+  RoomItemTypeMusicText,
+  RoomJoinLink,
+  RoomListItem,
+  RoomPartyGraphic,
+} from './styles'
+
+type Props = {
+  room: Room
+}
+
+export const RoomItem = ({ room }: Props) => {
+  return (
+    <RoomListItem>
+      <RoomPartyGraphic />
+      <RoomItemHeading>{room.title}</RoomItemHeading>
+      <RoomItemTypeMusicText>{room.typeOfMusic}</RoomItemTypeMusicText>
+      <Link passHref href={`/rooms/${room.id}`}>
+        <RoomJoinLink>Join</RoomJoinLink>
+      </Link>
+      <Link passHref href={`/profile/${room.owner}`}>
+        <RoomAuthorLink>by {room.ownerFullname}</RoomAuthorLink>
+      </Link>
+      <MusicalNote
+        css={{
+          left: 0,
+          transform: 'translate(10px, -10px)',
+          '@tablet': { transform: 'translate(15px, -15px)' },
+        }}
+      />
+      <MusicalNote
+        css={{
+          right: 0,
+          transform: 'translate(-10px, -10px)',
+          '@tablet': { transform: 'translate(-15px, -15px)' },
+        }}
+      />
+    </RoomListItem>
+  )
+}
---
+++ components/RoomItem/styles.ts
@@ -0,0 +1,117 @@
+import { styled } from 'stitches.config'
+import { RoomPartyIcon } from '@icons/RoomParty'
+import { MusicalNoteIcon } from '@icons/MusicalNote'
+import { toRem } from '@lib/helpers'
+import { keyframes } from '@stitches/react'
+
+const upDownAnim = keyframes({
+  '0%': {
+    transform: 'translateY(2px)',
+  },
+  '100%': {
+    transform: 'translateY(-2px)',
+  },
+})
+
+export const RoomListItem = styled('li', {
+  minHeight: 220,
+  width: 240,
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  boxShadow: '$shadowElevationLow',
+  position: 'relative',
+  border: '2px solid $secondary',
+  borderTop: 'none',
+  paddingBottom: 8,
+  '&:last-of-type': {
+    marginBottom: 16,
+  },
+  '@tablet': {
+    '&:last-of-type': {
+      marginBottom: 0,
+    },
+  },
+  '@laptop': {
+    minHeight: 320,
+    width: 300,
+  },
+})
+
+export const RoomItemHeading = styled('h2', {
+  color: '$secondary',
+  fontWeight: '$semiBold',
+  fontSize: toRem(30),
+  marginTop: 10,
+  textAlign: 'center',
+  '@laptop': {
+    fontSize: toRem(40),
+  },
+})
+
+export const RoomItemTypeMusicText = styled('p', {
+  fontSize: toRem(15),
+  fontWeight: '$medium',
+  color: '$tertiary',
+  marginTop: 5,
+  textAlign: 'center',
+  paddingX: 10,
+  '@laptop': {
+    fontSize: toRem(18),
+    marginTop: 10,
+  },
+})
+
+export const RoomJoinLink = styled('a', {
+  width: 84,
+  height: 26,
+  fontSize: toRem(16),
+  fontWeight: '$semiBold',
+  color: '$primary',
+  marginTop: 20,
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  backgroundColor: '$secondary',
+  boxShadow: '0 2px 2px rgba(0, 0, 0, 0.4)',
+  '@laptop': {
+    transition: 'all ease-out 0.2s',
+    marginTop: 'auto',
+    width: 100,
+    height: 35,
+    fontSize: toRem(22),
+    '&:hover': {
+      transform: 'scale(1.01) translateY(-2px)',
+      boxShadow: '0 3px 2px rgba(0, 0, 0, 0.5)',
+    },
+  },
+})
+
+export const RoomAuthorLink = styled('a', {
+  color: '$tertiary',
+  marginTop: 10,
+  fontWeight: '$regular',
+  fontSize: toRem(12),
+  textDecoration: 'underline',
+  '@laptop': {
+    marginTop: 10,
+    fontSize: toRem(14),
+  },
+})
+
+export const RoomPartyGraphic = styled(RoomPartyIcon, {
+  heightWidth: 80,
+  animation: `${upDownAnim} 1s linear infinite alternate`,
+  '@laptop': {
+    heightWidth: 110,
+  },
+})
+
+export const MusicalNote = styled(MusicalNoteIcon, {
+  position: 'absolute',
+  bottom: 0,
+  heightWidth: 20,
+  '@laptop': {
+    heightWidth: 30,
+  },
+})
